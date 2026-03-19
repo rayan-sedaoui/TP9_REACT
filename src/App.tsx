ffa8c3975371964800f00c3d58ef97d67a9b04ef
Rayan <ton@email.com>
2026-03-19 15:41:01 +0000

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="p-10 bg-zinc-900 text-white min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-500">
+        Tailwind est opérationnel ! 🚀
+      </h1>
+    </div>
+  )
+}
